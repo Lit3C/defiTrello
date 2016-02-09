7be3a67d3199927e61b298229d15ca3e24b85b0b
simplon-linap <lina.pich@gmail.com>
2016-02-09 15:59:26 +0100

    3rd save

--- requestTask.php
+++ requestTask.php
@@ -1,36 +1,21 @@
 <?php
-    // try
-    // {
-    //     $connexion = new PDO('mysql:host=localhost; dbname=defiTrello;charset=utf8', 'root', 'pàoçi_uè!:;,');
-    // } catch ( Exception $e ){
-    //     die('Erreur : '.$e->getMessage() );
-    // }
-    // $recupLog=$connexion->quote($recupLog); // équivaut à .$recupLog.
-    // $recupMdp=$connexion->quote($recupMdp); // équivaut à .$recupMdp.
-    //
-    // $requete = "SELECT * FROM auteurs WHERE nom=$recupLog AND mot_passe=$recupMdp";
-    // $reponses = $connexion->query($requete);
-    // if( $auteur = $reponses->fetch() ){
-    //   echo "GG it works !";
-    // }else{echo "Retry Again ;)";}
+  $recupTask = $_GET["task"];
 
-      $recupTask = $_GET["task"];
+try
+{
+    $connexion = new PDO('mysql:host=localhost; dbname=*********;charset=utf8', '******', '*********');
+} catch ( Exception $e ){
+    die('Erreur : '.$e->getMessage() );
+}
+$recupTask=$connexion->quote($recupTask);
 
-    try
-    {
-        $connexion = new PDO('mysql:host=localhost; dbname=*********;charset=utf8', '******', '*********');
-    } catch ( Exception $e ){
-        die('Erreur : '.$e->getMessage() );
-    }
-    $recupTask=$connexion->quote($recupTask);
+$requeteSaisie = "INSERT INTO taskList (`id`,`task`) VALUES ( NULL, $recupTask)";
+$sendDB = $connexion->query($requeteSaisie);
 
-    $requeteSaisie = "INSERT INTO taskList (`id`,`task`) VALUES ( NULL, $recupTask)";
-    $sendDB = $connexion->query($requeteSaisie);
+$requeteAffiche = "SELECT * FROM taskList";
+$resultats = $connexion->query($requeteAffiche);
+while( $list = $resultats->fetch() ){
+  echo "<li>".$list["task"]."</li>";
+}
 
-    $requeteAffiche = "SELECT * FROM taskList";
-    $resultats = $connexion->query($requeteAffiche);
-    while( $list = $resultats->fetch() ){
-      echo "<li>".$list["task"]."</li>";
-    }
-
-    ?>
+?>
